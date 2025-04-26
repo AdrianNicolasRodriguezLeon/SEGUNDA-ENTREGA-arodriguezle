870db6f5e260554894703fe63449e47d91a4cc38
Adrián Nicolás Rodríguez León <arodriguezle@unal.edu.co>
2025-04-26 15:57:04 -0500

    tercera version (correcion primer punto)

--- segunda_entregau.py
+++ segunda_entregau.py
@@ -11,17 +11,13 @@ def potencial(x,y):
     f= x**y
     return f
 
-
 def volumen_total(r1,r2,h):
 
     V_esfera= math.pi * potencial(r1,3) * 4/3
-    print("Este es el volumen de la esfera: ", V_esfera)
     V_cono= math.pi * potencial(r2,2) * 1/3 * h
-    print("Este es el volumen del cono: ", V_cono)
     VOLUMEN_TOTAL = V_esfera + V_cono
+    print("Este es el volumen de la esfera: ", V_esfera, " y este el volumen del cono: ", V_cono)
+    print("Por lo tanto este es el volumen total: ", VOLUMEN_TOTAL)
     return VOLUMEN_TOTAL
 
 volumen_total(3,4,9/2)
-print("El volumen total es: ", volumen_total(3,4,9/2))
-
-
